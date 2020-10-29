8444971aeb7ae85b2735bab3c296c26987732b51
oussama_bougy <oussama.bougaila@gmail.com>
2020-10-29 14:22:48 +0100

    feat: Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+'use strict'
+module.exports = {
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true,
+        jest: true
+    },
+    extends: [
+        'usecases/usecase/nodejs'
+    ],
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    rules: {
+        'object-curly-spacing': [2, 'always']
+    },
+}
